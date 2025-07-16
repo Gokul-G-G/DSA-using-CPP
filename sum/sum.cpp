d014de77e7d11310708bc2d1930e246a1d4ea257
Gokul Gopi <gokulraja143@gmail.com>
2025-07-16 23:30:04 +0530

    new program file created for sum

---
+++ sum/sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int num1, num2, sum ; //Declare variables
+    cout<<"Enter two number to get sum = \n";
+    cin>>num1>>num2; //Get value from user
+    sum = num1 + num2; 
+    cout<<"The sum of given numbers is = "<<sum;
+    return 0;
+}
