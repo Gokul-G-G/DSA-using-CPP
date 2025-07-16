deb6be28a61a5f9584d1df6d4d6dad208beef999
Gokul Gopi <gokulraja143@gmail.com>
2025-07-16 23:08:13 +0530

    sample cpp file created

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Execution starts here..
+int main() {
+    cout << "Hello Guys \nIts me Gokul" ;
+    return 0;
+}
+
+// 1 byte have 8 bits
+// 4 bytes (int)- ie; 32 bits 
+// 1 byte (char)- ie; 8 bits (Only stored on letter, only stored charectors ASCII value)
+// 4 bytes (float)- ie; 32 bits (used for floating value like 3.14)
+// 1 byte (bool)- ie; 8 bits (used for true or false 0/1)
+// 8 byte (double)- ie; 64 bits (used for big numbers)
